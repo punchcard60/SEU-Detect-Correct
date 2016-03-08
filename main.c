79cebc26a6a673c11e88bcc342ba86b0af86b2db
punchcard60 <punchcard60@homesw.net>
2016-03-07 20:42:08 -0600

    fixes

--- main.c
+++ main.c
@@ -28,6 +28,7 @@ int main(void) {
     // --TODO blink some LEDs to indicate fatal system error
   }
 
+  printf("System Crash!\n");
   for (;;);
 }
 
@@ -73,43 +74,13 @@ void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char *pcTaskName) 
 
 void test_FPU_test(void* p) {
   float ff = 1.0f;
-  printf("Start FPU test task.\n");
   for (;;) {
+  	printf("FPU test.\n");
     float s = sinf(ff);
     ff += s;
     // TODO some other test
-    vTaskDelay(1000);
+    vTaskDelay(2000);
   }
   vTaskDelete(NULL);
-}
-
-/*
- * Configure USART3(PB10, PB11) to redirect printf data to host PC.
- */
-void init_USART3(void) {
-  GPIO_InitTypeDef GPIO_InitStruct;
-  USART_InitTypeDef USART_InitStruct;
-
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-
-  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
-  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
-  GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_USART3);
-
-  USART_InitStruct.USART_BaudRate = 115200;
-  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-  USART_InitStruct.USART_StopBits = USART_StopBits_1;
-  USART_InitStruct.USART_Parity = USART_Parity_No;
-  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-  USART_Init(USART3, &USART_InitStruct);
-  USART_Cmd(USART3, ENABLE);
 }
 
